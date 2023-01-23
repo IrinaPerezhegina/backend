e7f6d8726d5667cf7c3ca6547f3a1c0c4297d421
IrinaPerezhegina <perezeginaiv@mail.ru>
2023-01-23 15:48:30 +0500

    HW_1

---
+++ index.js
@@ -0,0 +1,40 @@
+const yargs = require("yargs");
+const {
+  addNote,
+  getNotes,
+  printNotes,
+  removeNote,
+} = require("./notes.controller");
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove note by id",
+  handler({ id }) {
+    removeNote(id);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes();
+  },
+});
+
+yargs.parse();
---
+++ notes.controller.js
@@ -0,0 +1,49 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+  notes.push(note);
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.bgGreen("Note was added"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.bgBlue("Here is the list of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.id, note.title));
+  });
+}
+async function removeNote(id) {
+  const notes = await getNotes();
+  const removeElementIndex = notes.findIndex((el) => el.id === id.toString());
+  if (removeElementIndex !== -1) {
+    notes.splice(removeElementIndex, 1);
+    console.log(chalk.red(`Remove note by id=${id}`));
+  } else {
+    console.log(
+      chalk.green(`
+    Note with this id=${id} is missing`)
+    );
+  }
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+};
